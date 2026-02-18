c56f4a091c4d5b1106d998afd7ed819986a79e7f
juanje <juanjesusanchezrodriguez@hotmail.com>
2026-02-18 20:01:17 +0100

    Apartado 10 finalizado: Firewall operativo y acceso IDP validado

--- gw/firewall/firewall.sh
+++ gw/firewall/firewall.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -x
 
+# 0. Limpiar conexiones previas en memoria (evita que Google siga funcionando por sesiones viejas)
+sudo conntrack -F
+
 # Activamos el IP forwarding
 sudo sysctl -w net.ipv4.ip_forward=1
 
@@ -36,15 +39,13 @@ iptables -A INPUT -i eth3 -s 172.2.10.0/24 -p icmp --icmp-type echo-request -j A
 iptables -A OUTPUT -o eth2 -s 172.1.10.1 -p icmp --icmp-type echo-reply -j ACCEPT
 iptables -A OUTPUT -o eth3 -s 172.1.10.1 -p icmp --icmp-type echo-reply -j ACCEPT
 
-# L4. Permitir consultas DNS
+# L4. Permitir consultas DNS (Local)
 iptables -A OUTPUT -o eth0 -p udp --dport 53 -m conntrack --ctstate NEW -j ACCEPT
 iptables -A INPUT -i eth0 -p udp --sport 53 -m conntrack --ctstate ESTABLISHED -j ACCEPT
 
-# L5. Permitir http/https para actualizar y navegar
-iptables -A OUTPUT -o eth0 -p tcp --dport 80 -m conntrack --ctstate NEW,ESTABLISHED   -j ACCEPT
-iptables -A INPUT -i eth0 -p tcp --sport 80 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-iptables -A OUTPUT -o eth0 -p tcp --dport 443 -m conntrack --ctstate NEW,ESTABLISHED   -j ACCEPT
-iptables -A INPUT -i eth0 -p tcp --sport 443 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+# L5. Permitir http/https para actualizar y navegar (Local)
+iptables -A OUTPUT -o eth0 -p tcp -m multiport --dports 80,443 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+iptables -A INPUT -i eth0 -p tcp -m multiport --sports 80,443 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
 # L6. Permitir acceso SSH sólo desde adminpc
 iptables -A INPUT -i eth3 -s 172.2.10.10 -p tcp --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
@@ -53,67 +54,49 @@ iptables -A OUTPUT -o eth3 -d 172.2.10.10 -p tcp --sport 22 -m conntrack --ctsta
 ################################
 # REGLAS DE PROTECCIÓN DE RED
 ################################
-# R1. Se debe hacer NAT del tráfico saliente
-iptables -t nat -A POSTROUTING -s 172.2.10.0/24 -o eth0 -j MASQUERADE
 
-#R2. Permitir acceso desde la WAN a www a través del 80 haciendo port forwading
-# Redirige el tráfico que llega a la IP de la WAN (eth1) hacia el servidor www
+# --- BLOQUEO PREVENTIVO (NUEVO ORDEN - PRIORIDAD MÁXIMA) ---
+# Si un paquete de la LAN intenta ir directamente a la WEB (80,443), RECHAZO inmediato.
+iptables -A FORWARD -s 172.2.10.0/24 -p tcp -m multiport --dports 80,443 -j REJECT --reject-with icmp-admin-prohibited
+
+# R1. NAT del tráfico saliente (Solo para la DMZ/Squid)
+iptables -t nat -A POSTROUTING -s 172.1.10.0/24 -o eth1 -j MASQUERADE
+
+# R2. Port Forwarding a WWW (172.1.10.3)
 iptables -t nat -A PREROUTING -i eth1 -p tcp --dport 80 -j DNAT --to-destination 172.1.10.3
-
-# Permitir el paso en FORWARD para ese tráfico DNAT
 iptables -A FORWARD -i eth1 -o eth2 -p tcp -d 172.1.10.3 --dport 80 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 iptables -A FORWARD -i eth2 -o eth1 -p tcp -s 172.1.10.3 --sport 80 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
+# R3. Usuarios LAN -> WWW (DMZ) e SSH AdminPC
+iptables -A FORWARD -i eth3 -o eth2 -s 172.2.10.0/24 -d 172.1.10.3 -p tcp -m multiport --dports 80,443 -j ACCEPT
+iptables -A FORWARD -i eth3 -o eth2 -s 172.2.10.10 -d 172.1.10.3 -p tcp --dport 22 -j ACCEPT
+iptables -A FORWARD -i eth2 -o eth3 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
-# R3.a Usuarios LAN -> WWW (80 y 443)
-iptables -A FORWARD -i eth3 -o eth2 -s 172.2.10.0/24 -d 172.1.10.3 -p tcp -m multiport --dports 80,443 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i eth2 -o eth3 -s 172.1.10.3 -d 172.2.10.0/24 -p tcp -m multiport --sports 80,443 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+# R4.v2 OBLIGAR PROXY (Tráfico LAN a Internet)
+# 4.1 Permitir SOLO al puerto del Proxy (Squid)
+iptables -A FORWARD -i eth3 -o eth2 -s 172.2.10.0/24 -d 172.1.10.2 -p tcp --dport 3128 -j ACCEPT
 
-# R3.b AdminPC -> SSH a DMZ (Corregida)
-iptables -A FORWARD -i eth3 -o eth2 -s 172.2.10.10 -d 172.1.10.3 -p tcp --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i eth2 -o eth3 -s 172.1.10.3 -d 172.2.10.10 -p tcp --sport 22 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+# 4.2 DNS directo (Permitido para que resuelvan nombres)
+iptables -A FORWARD -i eth3 -o eth1 -s 172.2.10.0/24 -p udp --dport 53 -j ACCEPT
+# 4.3 NTP y Ping directos
+iptables -A FORWARD -i eth3 -o eth1 -s 172.2.10.0/24 -p udp --dport 123 -j ACCEPT
+iptables -A FORWARD -i eth3 -o eth1 -s 172.2.10.0/24 -p icmp --icmp-type echo-request -j ACCEPT
+iptables -A FORWARD -i eth1 -o eth3 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
-# R4. Permitir salir tráfico de la LAN
-iptables -A FORWARD -i eth3 -o eth0 -s 172.2.10.0/24 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i eth0 -o eth3 -d 172.2.10.0/24 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+# R5/P6. SALIDA SQUID (DMZ -> Internet)
+iptables -A FORWARD -i eth2 -o eth1 -s 172.1.10.2 -p tcp -m multiport --dports 80,443 -j ACCEPT
+iptables -A FORWARD -i eth1 -o eth2 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
-#R5. Permitir salir tráfico de la DMZ (sólo http/https/dns/ntp)
-# DNS (UDP 53)
-iptables -A FORWARD -i eth2 -o eth1 -s 172.1.10.0/24 -p udp --dport 53 -j ACCEPT
-iptables -A FORWARD -i eth1 -o eth2 -d 172.1.10.0/24 -p udp --sport 53 -j ACCEPT
-iptables -A FORWARD -i eth1 -o eth2 -d 172.1.10.0/24 -p udp --sport 123 -j ACCEPT
+# LDAP (172.2.10.2) hablando con el Proxy
+iptables -A FORWARD -i eth3 -o eth2 -s 172.2.10.2 -d 172.1.10.2 -p tcp --dport 3128 -j ACCEPT
 
-# HTTP/HTTPS y NTP (TCP 80,443 y UDP 123)
-iptables -A FORWARD -i eth2 -o eth1 -s 172.1.10.0/24 -p tcp -m multiport --dports 80,443 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i eth2 -o eth1 -s 172.1.10.0/24 -p udp --dport 123 -j ACCEPT
+# OPENVPN
+iptables -A INPUT -i eth1 -p udp --dport 1194 -j ACCEPT
+iptables -A OUTPUT -o eth1 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+# VPN -> DMZ y LDAP
+iptables -A FORWARD -i tun0 -s 172.3.10.0/24 -j ACCEPT
+iptables -A FORWARD -o tun0 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
-# Respuestas de Internet a la DMZ
-iptables -A FORWARD -i eth1 -o eth2 -d 172.1.10.0/24 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-
-# No olvides el NAT para la DMZ (para que puedan salir a internet)
-iptables -t nat -A POSTROUTING -s 172.1.10.0/24 -o eth1 -j MASQUERADE
-
-# Reglas para hacer logs - /var/log/kern.log
-iptables -A FORWARD -j LOG --log-prefix "JSR-FORWARD-"
-iptables -A INPUT -j LOG --log-prefix "JSR-INPUT-"
-iptables -A OUTPUT -j LOG --log-prefix "JSR-OUTPUT-"
-
-# =========================================================
-# REGLAS PARA OPENVPN (APARTADO 5)
-# =========================================================
-
-# Regla P4.2.1 Permitir acceso WAN (eth1) a servidor VPN
-iptables -A INPUT -i eth1 -p udp --dport 1194 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -o eth1 -p udp --sport 1194 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-
-# Permitir que el openvpn en el GW consulte al servidor LDAP (IDP) 
-iptables -A OUTPUT -o eth3 -d 172.2.10.2 -p tcp --dport 389 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
-iptables -A INPUT -i eth3 -s 172.2.10.2 -p tcp --sport 389 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-
-# Regla P4.2.2 Permitir acceso de VPN-net a http de la DMZ
-iptables -A FORWARD -i tun0 -o eth2 -s 172.3.10.0/24 -d 172.1.10.3 -p tcp -m multiport --dports 80,443 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i eth2 -o tun0 -s 172.1.10.3 -d 172.3.10.0/24 -p tcp -m multiport --sports 80,443 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-
-# Regla P4.2.3 Permitir acceso de VPN-net a IDP de la DMZ
-iptables -A FORWARD -i tun0 -o eth3 -s 172.3.10.0/24 -d 172.2.10.2 -p tcp --dport 389 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i eth3 -o tun0 -s 172.2.10.2 -d 172.3.10.0/24 -p tcp --sport 389 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+# LOGS (Al final para capturar lo rechazado)
+iptables -A FORWARD -j LOG --log-prefix "JSR-REJECT-FWD: "
+iptables -A INPUT -j LOG --log-prefix "JSR-REJECT-IN: "
